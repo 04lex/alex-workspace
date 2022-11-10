e0e62eb811ed42c8e1a54de40733583a7f3a5123
Lex <aalexandrua62@gmail.com>
2022-11-10 03:18:15 +0200

    sync

--- Lesson 3/calculatortva.py
+++ Lesson 3/calculatortva.py
@@ -1,5 +1,9 @@
-suma_fara_TVA = float(input("Introduceti suma fara TVA: "))
-TVA = 5
+suma_fara_TVA = float(input("Introduceti suma in LEI: "))
+TVA5 = 5
+TVA20 = 20
 
-suma_cu_TVA = suma_fara_TVA + 5/100 * suma_fara_TVA
-print(suma_cu_TVA)
+suma_cu_TVA5 = suma_fara_TVA + 5/100 * suma_fara_TVA
+suma_cu_TVA20 = suma_fara_TVA + 20/100 * suma_fara_TVA
+
+print("5% TVA din", suma_fara_TVA, "este: ", suma_cu_TVA5)
+print("20% TVA din", suma_fara_TVA, "este: ", suma_cu_TVA20)
