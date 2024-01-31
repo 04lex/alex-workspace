e36932ffe681b070969c839112787253ef9c2cbf
Daniel Lex <34891328+04lex@users.noreply.github.com>
2024-01-31 04:29:21 +0200

    pcep

---
+++ LINK Academy/PCEP/examen.py
@@ -0,0 +1,13 @@
+# def mysum(*numere):
+#     suma = 0
+#     for nr in numere:
+#         suma += nr
+#     return suma
+
+# print(mysum(1,2,3))
+
+
+
+def mysum(*numere):
+    suma = 0
+    return 
