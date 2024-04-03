476e9fcc15af15ae62877f03c0fc4227c7836aba
Daniel Lex <aalexandrua62@gmail.com>
2024-04-03 17:48:17 +0300

    React

---
+++ Internship Projects/Full Stack Web App/music_controller/frontend/src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import HomePage from "./homepage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+         <div>
+            <HomePage />
+         </div>
+        );
+    }
+}
+
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
---
+++ Internship Projects/Full Stack Web App/music_controller/frontend/src/components/homepage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import RoomJoinPage from './RoomJoinPage';
+import CreateRoomPage from './CreateRoomPage';
+import { BrowserRouter as Router, Routes, Route }from 'react-router-dom';
+
+
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Router>
+                <Routes>
+                    <Route path='/' element={<p>This is the homepage</p>} />
+
+                    <Route path='/join' element={<RoomJoinPage/>} />
+                    <Route path='/create' element={<CreateRoomPage/>} />
+                </Routes>
+            </Router>
+        );
+    }
+}
