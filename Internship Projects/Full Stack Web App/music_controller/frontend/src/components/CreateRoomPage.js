000f149da8202da3568bde2aea7ad52777d5c9f6
Daniel Lex <aalexandrua62@gmail.com>
2024-04-17 07:12:36 +0300

    react-api

--- Internship Projects/Full Stack Web App/music_controller/frontend/src/components/CreateRoomPage.js
+++ Internship Projects/Full Stack Web App/music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
-export default class CreateRoomPage extends Component {
-    constructor(props) {
-        super(props);
+// export default class CreateRoomPage extends Component {
+//     constructor(props) {
+//         super(props);
 
-    }
+//     }
 
-    render() {
-        return <p>This is the create room page</p>
-    }
+//     render() {
+//         return <p>This is the create room page</p>
+//     }
+// }
+
+function CreateRoomPage() {
+    return <div> This is create room page </div>
 }
+
+export default CreateRoomPage;
--- Internship Projects/Full Stack Web App/music_controller/frontend/src/components/homepage.js
+++ Internship Projects/Full Stack Web App/music_controller/frontend/src/components/homepage.js
@@ -5,21 +5,35 @@ import { BrowserRouter as Router, Routes, Route }from 'react-router-dom';
 
 
 
-export default class HomePage extends Component {
-    constructor(props) {
-        super(props);
-    }
+// export default class HomePage extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
 
-    render() {
-        return (
-            <Router>
-                <Routes>
-                    <Route path='/' element={<p>This is the homepage</p>} />
+//     render() {
+//         return (
+//             <Router>
+//                 <Routes>
+//                     <Route path='/' element={<p>This is the homepage</p>} />
 
-                    <Route path='/join' element={<RoomJoinPage/>} />
-                    <Route path='/create' element={<CreateRoomPage/>} />
-                </Routes>
-            </Router>
-        );
-    }
+//                     <Route path='/join' element={<RoomJoinPage/>} />
+//                     <Route path='/create' element={<CreateRoomPage/>} />
+//                 </Routes>
+//             </Router>
+//         );
+//     }
+// }
+
+function HomePage() {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<p>This is the homepage</p>} />
+                <Route path='/' element={<RoomJoinPage />} />
+                <Route path='/' element={<CreateRoomPage />} />
+            </Routes>
+        </Router>
+    );
 }
+
+export default HomePage;
